a4177f158ccd91a6c7ee9579ffc9c298e62c16b5
Nobody12here <uraja01313@gmail.com>
2025-05-11 23:10:20 -0700

    Updated the name of guest pass table

--- GuestPass/models.py
+++ GuestPass/models.py
@@ -15,5 +15,8 @@ class GuestPassRequest(models.Model):
     date_time = models.DateTimeField(name="date-time")
     created_at = models.DateField(auto_now_add=True)
 
+    class Meta:
+        db_table = "guest_pass_requests"
+
     def __str__(self):
         return f"Guest pass of {self.guest.name} "
