5ab59b385efaaa013672b7a8e875e25b6704af30
Nobody12here <uraja01313@gmail.com>
2025-05-13 22:01:52 -0700

    added guest pass functionality to the frontend

--- GuestPass/serializers.py
+++ GuestPass/serializers.py
@@ -9,10 +9,28 @@ class GuestPassRequestSerializer(ModelSerializer):
     name = serializers.CharField(write_only=True)
     phone_number = serializers.CharField(write_only=True)
 
+    # For GET (read-only from related guest)
+    guest_cnic = serializers.CharField(source="guest.CNIC", read_only=True)
+    guest_name = serializers.CharField(source="guest.name", read_only=True)
+    guest_phone = serializers.CharField(source="guest.phone_number", read_only=True)
+    host_department_name = serializers.CharField(
+        source="host_department.dept_name", read_only=True
+    )
+    host_name = serializers.CharField(
+        source="host_department.dept_head.name",read_only = True
+    )
+
     class Meta:
         model = GuestPassRequest
         fields = "__all__"
         read_only_fields = ["guest", "created_at"]
+        extra_kwargs = {
+            "guest_cnic_read": {"read_only": True},
+            "guest_name_read": {"read_only": True},
+            "guest_phone_read": {"read_only": True},
+            "host_name": {"read_only": True},
+            "host_department_name": {"read_only": True},
+        }
 
     def create(self, validated_data):
         # First check if the user with the given CNIC exists or not
