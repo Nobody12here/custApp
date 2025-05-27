007452b227af4a3cd34b5bd6eef005f2337f8653
Nobody12here <uraja01313@gmail.com>
2025-05-27 11:59:21 +0500

    Added filtering based on user type

--- GuestPass/views.py
+++ GuestPass/views.py
@@ -14,7 +14,7 @@ class RequestGuestPassView(ModelViewSet):
     def get_queryset(self):
         # Get today's date in the correct timezone
         user = self.request.user
-        base_queryset  = Request.objects.filter(request_type = "GuestPass")
+        base_queryset = Request.objects.filter(request_type="GuestPass")
         now = timezone.now()
         today = now.date()
 
@@ -22,29 +22,26 @@ class RequestGuestPassView(ModelViewSet):
         start_of_day = timezone.make_aware(datetime.combine(today, datetime.min.time()))
         end_of_day = start_of_day + timedelta(days=1)
 
-        if (user.dept == "Security" and user.dept.dept_head_id == user.id ):
-            #That means that the user is the head of security department
-            queryset  = base_queryset
+        if user.dept.dept_name == "Security" and user.dept.dept_head_id == user.user_id:
+            # That means that the user is the head of security department
+            print("head of security")
+            queryset = base_queryset
         else:
-            queryset = base_queryset.filter(ap)
-        queryset = (
-            base_queryset
-            .annotate(
-                is_today=Case(
-                    When(
-                        meeting_date_time__gte=start_of_day,
-                        meeting_date_time__lt=end_of_day,
-                        then=Value(True),
-                    ),
-                    default=Value(False),
-                    output_field=BooleanField(),
-                )
+            queryset = base_queryset.filter(host=user.user_id)
+        queryset = queryset.annotate(
+            is_today=Case(
+                When(
+                    meeting_date_time__gte=start_of_day,
+                    meeting_date_time__lt=end_of_day,
+                    then=Value(True),
+                ),
+                default=Value(False),
+                output_field=BooleanField(),
             )
-            .order_by(
-                "-is_today",  # Today's meetings first
-                "meeting_date_time",  # Then by meeting time (earliest first)
-                "-created_at",  # Finally by creation time (newest first)
-            )
+        ).order_by(
+            "-is_today",  # Today's meetings first
+            "meeting_date_time",  # Then by meeting time (earliest first)
+            "-created_at",  # Finally by creation time (newest first)
         )
 
         return queryset
