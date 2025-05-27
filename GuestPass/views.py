ff19821b638a8e92d94d7887c641d0a763f7bf34
Nobody12here <uraja01313@gmail.com>
2025-05-27 11:34:05 +0500

    Added media to gitignore

--- GuestPass/views.py
+++ GuestPass/views.py
@@ -13,21 +13,28 @@ class RequestGuestPassView(ModelViewSet):
 
     def get_queryset(self):
         # Get today's date in the correct timezone
+        user = self.request.user
+        base_queryset  = Request.objects.filter(request_type = "GuestPass")
         now = timezone.now()
         today = now.date()
-        
+
         # Compute start and end of today (timezone-aware)
         start_of_day = timezone.make_aware(datetime.combine(today, datetime.min.time()))
         end_of_day = start_of_day + timedelta(days=1)
 
+        if (user.dept == "Security" and user.dept.dept_head_id == user.id ):
+            #That means that the user is the head of security department
+            queryset  = base_queryset
+        else:
+            queryset = base_queryset.filter(ap)
         queryset = (
-            Request.objects.filter(request_type="GuestPass")
+            base_queryset
             .annotate(
                 is_today=Case(
                     When(
                         meeting_date_time__gte=start_of_day,
                         meeting_date_time__lt=end_of_day,
-                        then=Value(True)
+                        then=Value(True),
                     ),
                     default=Value(False),
                     output_field=BooleanField(),
