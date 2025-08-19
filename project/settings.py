ebfb0df8b74c77620b1b492351dba79f6eb04869
Nobody12here <uraja01313@gmail.com>
2025-08-19 13:17:04 +0500

    Update EMAIL_HOST to use Outlook SMTP server for email backend

--- project/settings.py
+++ project/settings.py
@@ -150,7 +150,7 @@ EMAIL_BACKEND = os.environ.get(
     "EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend"
 )  # For testing
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "localhost"
+EMAIL_HOST = "smtp.outlook.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
